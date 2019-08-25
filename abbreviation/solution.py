04b83cb1f5c157add4448f1cf6f757cddc440b43
Adnan Yaqoob <adnan.yaqoob@bhpbilliton.com>
2019-08-25 12:47:36 +0800

    updates abbreviation solution

--- abbreviation/solution.py
+++ abbreviation/solution.py
@@ -1,41 +1,31 @@
 #!/bin/python3
 
 import os
-import sys
-
-sys.setrecursionlimit(50000)
-
-dp = {}
-
-def memcheck(a, b):
-    result = dp.get((a, b), None)
-    if result == None:
-        result = check(a, b)
-        dp[(a, b)] = result
-    
-    return result
-
-def check(a, b):
-    if len(a) == 0 and len(b) == 0:
-        return True
-    elif len(b) > 0 and len(a) == 0:
-        return False
-    elif len(a) > 0 and len(b) == 0:
-        if a[0].islower():
-            return memcheck(a[1:], b)
-        else:
-            return False
-    else:
-        if a[0].isupper():
-            if a[0] != b[0]:
-                return False
-            else:
-                return memcheck(a[1:], b[1:])
-        else:
-            return memcheck(a[0].upper() + a[1:], b) or memcheck(a[1:], b)
 
 def abbreviation(a, b):
-    return "YES" if check(a, b) else "NO"
+    dp = [[False for _ in range(len(b) + 1)] for _ in range(len(a) + 1)]
+
+    dp[0][0] = True
+    
+    has_upper = False
+    for i in range(1, len(a) + 1):
+        dp[i][0] = True
+        if a[i - 1].isupper() or has_upper:
+            has_upper = True
+            dp[i][0] = False
+
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            if a[i - 1] == b[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            elif a[i - 1].isupper():
+                dp[i][j] = False
+            elif a[i - 1].upper() == b[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] or dp[i - 1][j]
+            else:
+                dp[i][j] = dp[i - 1][j]
+    
+    return "YES" if dp[len(a)][len(b)] else "NO"
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
